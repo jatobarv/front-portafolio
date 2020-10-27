022980673ddcffcf4a865a71c851e2f2b26faff3
Javier Tobar <ja.tobarv@alumnos.duoc.cl>
2020-10-27 15:37:10 -0300

    Añade Modificar funciones

--- js/funciones/funciones.js
+++ js/funciones/funciones.js
@@ -6,6 +6,16 @@ const d = document;
 var nCreadores;
 var nPags;
 var usuarios = [];
+var nFunciones;
+var datos = [];
+
+var $nombre = d.getElementById("select-nombre");
+var $descripcion = document.getElementById("select-descripcion");
+var $fechaInicio = document.getElementById("select-fecha_inicio");
+var $fechaTermino = d.getElementById("select-fecha_termino");
+var $porcentaje = d.getElementById("select-porcentaje");
+var $creador = d.getElementById("select-creador");
+var $unidad = d.getElementById("select-unidad");
 
 function getCreadores() {
     const promise = axios.get(`http://127.0.0.1:8000/usuarios/`, {
@@ -106,7 +116,7 @@ getUnidades()
     })
     .catch((err) => console.log(err));
 
-async function funcion(
+async function addFuncion(
     nombre,
     descripcion,
     fecha_inicio,
@@ -128,33 +138,199 @@ async function funcion(
             creador,
             unidad,
         },
-        action: "post unidades",
+        action: "post funciones",
     });
     localStorage.setItem("Token", token);
 
     if (response) {
-        localStorage.setItem("Token", response.token);
-        // location.replace("./principal.html");
+        // localStorage.setItem("Token", response.token);
+        location.replace("./funciones.html");
     } else {
         alert("Datos incorrectos");
     }
 }
 
-d.addEventListener("submit", (event) => {
+function getFunciones() {
+    const promise = axios.get(`http://127.0.0.1:8000/funciones/`, {
+        headers: {
+            Authorization: "Token " + token,
+            "Content-Type": "application/json",
+        },
+    });
+
+    const dataPromise = promise.then((response) => response.data);
+
+    return dataPromise;
+}
+let selEdit = document.getElementById("select-id").value;
+
+getFunciones().then((funciones) => {
+    nFunciones = funciones.count;
+    nPags = Math.ceil(nFunciones / 10);
+    
+    console.log("pags: " +nPags);
+    if (nPags === 0) {
+        nPags = 1;
+    }
+    for (let i = 1; i <= nPags; i++) {
+        axios
+            .get(`http://127.0.0.1:8000/funciones/?page=${i}`, {
+                headers: {
+                    Authorization: "Token " + token,
+                    "Content-Type": "application/json",
+                },
+            })
+            .then((res) => {
+                datos = res.data.results;
+                for (const funcion of datos) {
+                    var sel = document.getElementById("table-body");
+                    var tr = document.createElement("tr");
+                    var tdId = document.createElement("td");
+                    var tdName = document.createElement("td");
+                    var tdDescripcion = document.createElement("td");
+                    var tdFechaInicio = document.createElement("td");
+                    var tdFechaTermino = document.createElement("td");
+                    var tdPorcentaje = document.createElement("td");
+                    var tdUnidad = document.createElement("td");
+                    var tdCreador = document.createElement("td");
+                    var tdBtn = document.createElement("td");
+                    var btn = document.createElement("button");
+
+                    tdId.appendChild(document.createTextNode(funcion.id));
+                    tdName.appendChild(document.createTextNode(funcion.nombre));
+                    tdDescripcion.appendChild(
+                        document.createTextNode(funcion.descripcion)
+                    );
+                    tdFechaInicio.appendChild(
+                        document.createTextNode(funcion.fecha_inicio)
+                    );
+                    tdFechaTermino.appendChild(
+                        document.createTextNode(funcion.fecha_termino)
+                    );
+                    tdPorcentaje.appendChild(
+                        document.createTextNode(funcion.porcentaje_realizacion)
+                    );
+                    tdUnidad.appendChild(
+                        document.createTextNode(funcion.unidad)
+                    );
+                    tdCreador.appendChild(
+                        document.createTextNode(funcion.creador)
+                    );
+                    btn.className = "btn btn-secondary";
+                    btn.type = "submit";
+                    btn.id = "btn " + funcion.id;
+                    btn.appendChild(document.createTextNode("Editar"));
+
+                    tr.appendChild(tdId);
+                    tr.appendChild(tdName);
+                    tr.appendChild(tdDescripcion);
+                    tr.appendChild(tdFechaInicio);
+                    tr.appendChild(tdFechaTermino);
+                    tr.appendChild(tdPorcentaje);
+                    tr.appendChild(tdUnidad);
+                    tr.appendChild(tdCreador);
+                    tr.appendChild(tdBtn);
+                    tdBtn.appendChild(btn);
+                    sel.appendChild(tr);
+
+                    btn.addEventListener("click", (event) => {
+                        event.preventDefault();
+                        var selNombre = document.getElementById(
+                            "select-nombre"
+                        );
+                        var selDescripcion = document.getElementById(
+                            "select-descripcion"
+                        );
+                        var selFechaInicio = document.getElementById(
+                            "select-fecha_inicio"
+                        );
+                        var selFechaTermino = document.getElementById(
+                            "select-fecha_termino"
+                        );
+                        var selPorcentaje = document.getElementById(
+                            "select-porcentaje"
+                        );
+                        var selCreador = document.getElementById(
+                            "select-creador"
+                        );
+                        var selUnidad = document.getElementById(
+                            "select-unidad"
+                        );
+                        var selid = document.getElementById("select-id");
+                        (selid.value = funcion.id),
+                            (selNombre.value = funcion.nombre),
+                            (selDescripcion.value = funcion.descripcion),
+                            (selFechaInicio.value = funcion.fecha_inicio),
+                            (selFechaTermino.value = funcion.fecha_termino),
+                            (selPorcentaje.value =
+                                funcion.porcentaje_realizacion),
+                            (selCreador.value = funcion.creador),
+                            (selUnidad.value = funcion.unidad);
+                        selEdit = funcion.id;
+                    });
+                }
+            });
+    }
+    for (const funcion of funciones.results) {
+        console.log(funcion);
+    }
+});
+
+async function editFuncion(
+    nombre,
+    descripcion,
+    fecha_inicio,
+    fecha_termino,
+    porcentaje_realizacion,
+    creador,
+    unidad
+) {
+    const response = await apiRequest({
+        url: `http://127.0.0.1:8000/funciones/${selEdit}/`,
+        method: "PUT",
+        token: token,
+        body: {
+            nombre,
+            descripcion,
+            fecha_inicio,
+            fecha_termino,
+            porcentaje_realizacion,
+            creador,
+            unidad,
+        },
+        action: "put funciones",
+    });
+    localStorage.setItem("Token", token);
+
+    if (response) {
+        location.replace("./funciones.html");
+    } else {
+        alert("Datos incorrectos");
+    }
+}
+
+d.addEventListener("click", (event) => {
     event.preventDefault();
     const target = event.target;
-
-    var selCreador = document.getElementById("select-creador");
-    var selUnidad = document.getElementById("select-unidad");
-    if (target.id === "unidades") {
-        funcion(
-            target.nombre.value,
-            target.descripcion.value,
-            target.fecha_inicio.value,
-            target.fecha_termino.value,
-            target.porcentaje_realizacion.value,
-            selCreador.value,
-            selUnidad.value
+    if (target.id === "modificar_tarea") {
+        editFuncion(
+            $nombre.value,
+            $descripcion.value,
+            $fechaInicio.value,
+            $fechaTermino.value,
+            $porcentaje.value,
+            $creador.value,
+            $unidad.value
+        );
+    } else if (target.id === "agregar_tarea") {
+        addFuncion(
+            $nombre.value,
+            $descripcion.value,
+            $fechaInicio.value,
+            $fechaTermino.value,
+            $porcentaje.value,
+            $creador.value,
+            $unidad.value
         );
     }
 });
